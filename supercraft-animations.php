d2b64988e206da9cb4c0c1bd5ea2749d313a2708
Lyness Lim <lynesslim@gmail.com>
2026-06-30 22:33:27 +0800

    Bump version to 0.3.1

--- supercraft-animations.php
+++ supercraft-animations.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Superanimate GSAP Elementor
  * Description: GSAP-based animation presets with Elementor controls.
- * Version: 0.3.0
+ * Version: 0.3.1
  * Author: Supercraft
  */
 
